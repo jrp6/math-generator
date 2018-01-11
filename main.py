c6df0dd0bcbd62a20021753e10a9c66698d41ef9
Juho Rinta-Paavola <juhorp@juhorp.fi>
2018-01-11 11:50:33 +0200

    Condense layout
    - Make top and bottom margins smaller
    - Remove page numbers
    - Make title shorter
    - Increase amount of tasks to fill the whole two pages

--- main.py
+++ main.py
@@ -30,13 +30,15 @@ print("""
 \\usepackage{ae,aecompl}
 \\usepackage{amsmath}
 \\usepackage[finnish]{babel}
-\\pagestyle{plain}
+\\usepackage[top=1cm, bottom=2cm, left=3cm, right=3cm]{geometry}
+\\pagestyle{empty}
 
 \\begin{document}
 
-\\title{Musiikin vaikutus kognitiiviseen suorituskykyyn:\\\\
-  Matemaattinen osio}
+\\title{Matemaattinen osio}
+\\date{}
 \\maketitle
+\\thispagestyle{empty}
 """)
-printRandomExercises(47)
+printRandomExercises(70)
 print("\\end{document}")
